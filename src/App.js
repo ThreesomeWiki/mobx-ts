1e3f15fe4d440ff0e55a0d2c01700d004b5d1385
hanhuizhu <hanhuizhuxyl@gmail.com>
2020-06-18 14:06:33 +0800

    refact:优化mainscreen

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 import { observer, inject } from "mobx-react";
 import { Tab, TabBarItem, Article } from "react-weui";
 import Icon from "./components/iconfont";
+import Home from "./page/home/Home";
 @inject("dataStore")
 @observer
 class App extends React.Component {
   state = { show: false, tab: 0 };
+
+  tabs = [
+    { label: "店主", icon: "heart", Component: Home },
+    { label: "货源", icon: "heart", Component: Home },
+    { label: "订单", icon: "heart", Component: Home },
+    { label: "培训", icon: "heart", Component: Home },
+    { label: "我的", icon: "heart", Component: Home },
+  ];
+
   render() {
     const { dataStore = {} } = this.props;
     const { tab } = this.state;
-
     return (
       <div className="App">
         <Tab
@@ -18,38 +27,21 @@ class App extends React.Component {
             this.setState({ tab: index });
           }}
         >
-          <TabBarItem
-            icon={
-              <Icon
-                name="heart"
-                size={0.27}
-                color={tab === 0 ? "#09BB07" : "#ddd"}
-              />
-            }
-            label="Tab1"
-          >
-            <span>123</span>
-          </TabBarItem>
-          <TabBarItem
-            icon={
-              <Icon
-                name="heart"
-                size={0.27}
-                color={tab === 1 ? "#09BB07" : "#ddd"}
-              />
-            }
-            label="Tab2"
-          ></TabBarItem>
-          <TabBarItem
-            icon={
-              <Icon
-                name="heart"
-                size={0.27}
-                color={tab === 2 ? "#09BB07" : "#ddd"}
-              />
-            }
-            label="Tab3"
-          ></TabBarItem>
+          {this.tabs.map((Item, index) => (
+            <TabBarItem
+              key={Item.label}
+              icon={
+                <Icon
+                  name={Item.icon}
+                  size={0.27}
+                  color={tab === index ? "#09BB07" : "#ddd"}
+                />
+              }
+              label={Item.label}
+            >
+              <Item.Component />
+            </TabBarItem>
+          ))}
         </Tab>
       </div>
     );
